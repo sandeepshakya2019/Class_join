77c7e123de93debdd22ec0eaf856ed2c95925a81
Sandeep Kumar <51978973+sandeepshakya2019@users.noreply.github.com>
2020-08-26 13:35:01 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,11 @@
+date1 = new Date()
+day = date1.getDay()
+
+var x = document.getElementsByClassName(day);
+var set = x[0].classList.add("yes")
+x[0].classList.add("container")
+x[0].classList.add("border")
+
+
+
+
